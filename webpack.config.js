9996976b96758b27fd710dfd4d93844fdfed2932
kabuspl <kabuspl@outlook.com>
2023-02-05 21:09:17 +0100

    add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.ts',
@@ -18,7 +19,8 @@ module.exports = {
         patterns: [
             { from: 'src/static' }
         ]
-    })
+    }),
+    new Dotenv()
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
