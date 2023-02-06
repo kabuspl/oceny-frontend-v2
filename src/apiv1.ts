236a4f1fade45249b52acb6e9cbbb7104b19597f
kabuspl <kabuspl@outlook.com>
2023-02-06 01:55:57 +0100

    add getting latest day to apiv1

--- src/apiv1.ts
+++ src/apiv1.ts
@@ -1,8 +1,14 @@
 import { ApiData, DayDiff } from "./types";
 
 export async function getDayDiffForDate(date: Date) {
-    let data: ApiData = await fetch(process.env.API_ENDPOINT+"/v1/getDayDiffForDate").then(data=>data.json());
+    let data: ApiData = await fetch(process.env.API_ENDPOINT+"/v1/getDayDiffForDate/"+date.toISOString().substring(0,10)).then(data=>data.json());
     if(!data.success) return false;
     let daydiff: DayDiff = data.data;
     return daydiff;
+}
+
+export async function getLatestDate() {
+    let data: ApiData = await fetch(process.env.API_ENDPOINT+"/v1/getDayDiff").then(data=>data.json());
+    let latestDate = new Date(Object.keys(data.data).at(-1));
+    return latestDate;
 }
