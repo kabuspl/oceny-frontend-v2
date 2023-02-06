d9918a09a1ac54a3057cc9e7f75d5890444c978f
kabuspl <kabuspl@outlook.com>
2023-02-06 01:56:21 +0100

    add basic ui handling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-alert("Hello world!");
+import { getDayDiffForDate, getLatestDate } from "./apiv1";
+import { DayDiff } from "./types";
+
+const gradeCounter = document.querySelector("#grades-count");
+const gradeDetails = document.querySelector("#grades-details");
+
+async function start() {
+    let latestDate = await getLatestDate();
+    updateUI(latestDate);
+}
+
+async function updateUI(date: Date) {
+    let data = await getDayDiffForDate(date);
+    if(!data) return false;
+    let daydiff: DayDiff = data;
+    gradeCounter.textContent = daydiff["Matematyka"][1].toString();
+}
+
+start();
