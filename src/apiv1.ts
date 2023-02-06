8b78c369aafd86ec8fc058030d929ce6ed59f031
kabuspl <kabuspl@outlook.com>
2023-02-06 20:41:23 +0100

    add getFullDayDiff()

--- src/apiv1.ts
+++ src/apiv1.ts
@@ -1,4 +1,4 @@
-import { ApiData, DayDiff } from "./types";
+import { ApiData, DayDiff, FullDayDiff } from "./types";
 
 export async function getDayDiffForDate(date: Date) {
     let data: ApiData = await fetch(process.env.API_ENDPOINT+"/v1/getDayDiffForDate/"+date.toISOString().substring(0,10)).then(data=>data.json());
@@ -17,4 +17,11 @@ export async function getFirstDate() {
     let data: ApiData = await fetch(process.env.API_ENDPOINT+"/v1/getDayDiff").then(data=>data.json());
     let firstDate = new Date(Object.keys(data.data).at(0));
     return firstDate;
+}
+
+export async function getFullDayDiff() {
+    let data: ApiData = await fetch(process.env.API_ENDPOINT+"/v1/getArchivalDayDiff").then(data=>data.json());
+    if(!data.success) return false;
+    let fulldaydiff: FullDayDiff = data.data as FullDayDiff;
+    return fulldaydiff;
 }
