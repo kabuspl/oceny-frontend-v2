0ac373e03f14aaecc5463453b1ddf403885a4495
kabuspl <kabuspl@outlook.com>
2023-02-06 02:37:57 +0100

    make back and forward buttons working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
-import { getDayDiffForDate, getLatestDate } from "./apiv1";
+import { getDayDiffForDate, getFirstDate, getLatestDate } from "./apiv1";
 import { DayDiff } from "./types";
 
 const gradeCounter = document.querySelector("#grades-count");
 const gradeDetails = document.querySelector("#grades-details");
 
+const backArrow = document.querySelector("#back");
+const dateDisplay = document.querySelector("#date-display");
+const forwardArrow = document.querySelector("#forward");
+
+let currentDate: Date, latestDate: Date, firstDate: Date;
+
+backArrow.addEventListener("click",()=>{
+    let tempdate = new Date(currentDate.getTime());
+    tempdate.setDate(currentDate.getDate()-1);
+    if(tempdate<firstDate) return;
+    currentDate.setDate(currentDate.getDate()-1)
+    dateDisplay.textContent = currentDate.toISOString().substring(0,10)
+    updateUI(currentDate);
+});
+forwardArrow.addEventListener("click",()=>{
+    let tempdate = new Date(currentDate.getTime());
+    tempdate.setDate(currentDate.getDate()+1);
+    if(tempdate>latestDate) return;
+    currentDate.setDate(currentDate.getDate()+1)
+    dateDisplay.textContent = currentDate.toISOString().substring(0,10)
+    updateUI(currentDate);
+});
+
 async function start() {
-    let latestDate = await getLatestDate();
-    updateUI(latestDate);
+    latestDate = await getLatestDate();
+    firstDate = await getFirstDate();
+    currentDate = new Date(latestDate.getTime());
+    dateDisplay.textContent = currentDate.toISOString().substring(0,10)
+    updateUI(currentDate);
 }
 
 async function updateUI(date: Date) {
